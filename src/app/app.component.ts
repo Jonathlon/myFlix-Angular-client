d0224bd9c2172fb0837dfe5c6205ce4182a4905b
Jon Heaney <113617821+Jonathlon@users.noreply.github.com>
2023-09-19 17:17:07 +0100

    aded user registration form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,13 @@ export class AppComponent {
   title = 'myFlix-Angular-client';
 
 
-constructor(public dialog: MatDialog) { }
-// This is the function that will open the dialog when the signup button is clicked  
-openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-// Assigning the dialog a width
-    width: '280px'
-    });
-  }
+  constructor(public dialog: MatDialog) { }
+  // This is the function that will open the dialog when the signup button is clicked  
+  openUserRegistrationDialog(): void {
+      this.dialog.open(UserRegistrationFormComponent, {
+  // Assigning the dialog a width
+      width: '280px'
+      });
+    }
+
 }
